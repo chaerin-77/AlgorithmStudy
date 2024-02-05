9983547d9f40f45b5cee7af4bc603898f9be6f6e
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-05 14:50:22 +0900

    [Gold V] Title: 탑, Time: 3244 ms, Memory: 176980 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -4,29 +4,35 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+// 시간: 3340ms, 메모리: 178,340KB
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		
-		Stack<Integer> check = new Stack<>();
-		int[] top = new int[N + 1];
-		int[] reception = new int[N + 1];
+		Stack<Integer> check = new Stack<>(); // 높이를 확인할 스택
+		int[] top = new int[N + 1]; // 값을 입력 받을 배열
+		int[] reception = new int[N + 1]; // 정답을 저장할 배열
 		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
+			// 배열에 입력 값 저장
 			top[i] = Integer.parseInt(st.nextToken());
 		}
 		
+		// 첫 번째 요소의 인덱스는 먼저 push
 		check.push(1);
 		for (int i = 2; i <= N; i++) {
 			while (!check.empty()) {
+				// 만약 스택에 push된 인덱스로 접근한 값이 크거나 같다면 break;
 				if (top[i] <= top[check.peek()]) {
 					reception[i] = check.peek();
 					break;
 				}
+				// 그게 아니라면 pop
 				check.pop();
 			}
+			// 다음에 어떤 높이가 들어올지 모르기 때문에 현재 값 push
 			check.push(i);
 			
 		}
