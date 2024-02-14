6e473299504276181b2ab34d8eb0ad6b97fa1b19
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-14 17:36:21 +0900

    [D3] Title: N-Queen, Time: 121 ms, Memory: 20,488 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -30,11 +30,13 @@ public class Solution {
 		}
 		
 		A: for (int i = 0; i < N; i++) {
-			if (Ccheck[i]) continue;
-			board[r] = i;
+			if (Ccheck[i]) continue; // 해당 열에 값이 있으면 continue
+			board[r] = i; // r행의 i 열에 값이 있다는 의미
 			for (int j = 0; j < r; j++) {
+				// 대각선 체크 (기울기)
 				if (Math.abs(board[j] - board[r]) == Math.abs(j - r)) continue A;
 			}
+			// 열의 값을 true로 하고 dfs 후 다시 원상 복구
 			Ccheck[i] = true;
 			dfs(r + 1);
 			Ccheck[i] = false;
