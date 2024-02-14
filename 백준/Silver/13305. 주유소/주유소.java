4422c973c3b756025a580cb3dc4b4b7075b0e332
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-14 11:32:38 +0900

    [Silver III] Title: 주유소, Time: 372 ms, Memory: 41036 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -9,7 +9,7 @@ public class Main {
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
 		int[] distance = new int[N];
-		int[] oil = new int[N];
+		long[] oil = new long[N];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N - 1; i++) {
 			distance[i] = Integer.parseInt(st.nextToken());
@@ -18,10 +18,10 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			oil[i] = Integer.parseInt(st.nextToken());
 		}
-		int o = oil[0];
-		int answer = oil[0] * distance[0];
+		long o = oil[0];
+		long answer = oil[0] * distance[0];
 		for (int i = 1; i < N - 1; i++) {
-			o = Math.min(o, oil[i]);
+			if (o > oil[i]) o = oil[i];
 			answer += o * distance[i];
 		}
 		System.out.println(answer);
