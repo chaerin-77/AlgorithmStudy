b908997e7038f060c97cd8abb0bcd96c1cdc2802
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-15 14:28:23 +0900

    [Gold V] Title: 신기한 소수, Time: 512 ms, Memory: 13752 KB -BaekjoonHub

--- 백준/Gold V/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold V/2023. 신기한 소수/신기한 소수.java
@@ -16,11 +16,15 @@ public class Main {
 			return;
 		}
 		
+		// 맨 앞자리에는 1을 놓지 않기 위한 start 값 설정
 		int start = 1;
 		if (cnt == 0) start = 2;
+		
 		A: for (int i = start; i < 10; i++) {
+			// 다음 숫자 만들기
 			int temp = num * 10 + i;
 			for (int j = 2; j <= temp / 2; j++) {
+				// 나누어 떨어지는 수가 있다면 다음 숫자 생성
 				if(temp % j == 0) continue A;
 			}
 			rec(cnt + 1, temp);
