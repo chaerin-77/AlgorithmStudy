a54613cb99a236cc8d4a08338196817da5840333
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-16 10:38:57 +0900

    Update 스킬트리.java

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -5,16 +5,20 @@ class Solution {
         A: for (int i = 0; i < Scnt; i++) {
         	String sk = skill_trees[i];
         	int idx = 0;
-        	for (int j = 0; j < sk.length(); j++) {
+        	B: for (int j = 0; j < sk.length(); j++) {
         		for (int k = idx; k < skill.length(); k++) {
-        			if (sk.charAt(j) == skill.charAt(idx))idx++;
-        			else if (k > idx && sk.charAt(j) == skill.charAt(k))
+        			// 선행 스킬과 같다면 다음 스킬로 인덱스 증가 후 다음 탐색
+        			if (sk.charAt(j) == skill.charAt(idx)) {
+        				idx++;
+        				continue B;
+        			}
+        			// 선행 스킬이 아닌데 사용한 스킬이 있다면 다음 단어 탐색
+        			else if (sk.charAt(j) == skill.charAt(k))
         				continue A;
         		}
         	}
         	answer++;
         }
-        
         return answer;
     }
 }
