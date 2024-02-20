09a00fe7453c588675ca14a8aca2593d91e5a7f4
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-20 20:16:55 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 12876 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -7,7 +7,7 @@ import java.util.Deque;
 public class Main {
 	static int N, Ncnt = 0, Bcnt = 0;
 	static char[][] image;
-	static boolean[][] visitedN, visitedB;
+	static boolean[][] visited;
 	static int[] dr = {0, -1, 0, 1};
 	static int[] dc = {-1, 0, 1, 0};
 	
@@ -16,8 +16,7 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		
 		image = new char[N][N];
-		visitedN = new boolean[N][N];
-		visitedB = new boolean[N][N];
+		visited = new boolean[N][N];
 		for (int i = 0; i < N; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < N; j++) {
@@ -27,12 +26,21 @@ public class Main {
 		
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if (!visitedN[i][j]) {
-					Nbfs(i, j);
+				if (!visited[i][j]) {
+					bfs(i, j);
 					Ncnt++;
 				}
-				if (!visitedB[i][j]) {
-					Bbfs(i, j);
+
+				// 적록 색약이 아닌 사람 계산 후 적록 색약 계산을 위해 같은 색으로 변경
+				if (image[i][j] == 'G') image[i][j] = 'R';
+			}
+		}
+
+		visited = new boolean[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (!visited[i][j]) {
+					bfs(i, j);
 					Bcnt++;
 				}
 			}
@@ -40,9 +48,9 @@ public class Main {
 		System.out.println(Ncnt + " " + Bcnt);
 	}
 	
-	private static void Nbfs(int startR, int startC) {
+	private static void bfs(int startR, int startC) {
 		Deque<int[]> q = new ArrayDeque<>();
-		visitedN[startR][startC] = true;
+		visited[startR][startC] = true;
 		q.offer(new int[] {startR, startC});
 		
 		while(!q.isEmpty()) {
@@ -55,41 +63,15 @@ public class Main {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
 				
+				// 경계 체크
 				if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
-				if (visitedN[nr][nc] || image[nr][nc] != col) continue;
+				// 방문 여부, 색 동일 여부 체크
+				if (visited[nr][nc] || image[nr][nc] != col) continue;
 				
-				visitedN[nr][nc] = true;
+				visited[nr][nc] = true;
 				q.offer(new int[] {nr, nc});
 			}
 		}
 	}
-	
-	private static void Bbfs(int startR, int startC) {
-		Deque<int[]> q = new ArrayDeque<>();
-		visitedB[startR][startC] = true;
-		q.offer(new int[] {startR, startC});
-		
-		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			int r = temp[0];
-			int c = temp[1];
-			char col = image[r][c];
-			
-			for (int d = 0; d < 4; d++) {
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-				
-				if (nr < 0 || nc < 0 || nr >= N || nc >= N) continue;
-				if (visitedB[nr][nc]) continue;
-				
-				if (col == 'R' || col == 'G') {
-					if (image[nr][nc] != 'R' && image[nr][nc] != 'G') continue;
-				}
-				else if (image[nr][nc] != col) continue;
-				
-				visitedB[nr][nc] = true;
-				q.offer(new int[] {nr, nc});
-			}
-		}
-	}
+
 }
