c6043f2eacb762fe6261727362ca07e11888b7fc
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-20 14:59:12 +0900

    [Gold IV] Title: 알파벳, Time: 852 ms, Memory: 12408 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.StringTokenizer;
+
+/*
+ * 문제 해결 프로세스
+ * 1. bfs로 탐색을 할 경우 같은 레벨의 같은 알파벳을 확인 못함 -> dfs
+ * 2. 탐색 시마다 최대 방문 가능한 count 갯수 갱신
+ */
 
 public class Main {
 	static int R, C, count = 0;
@@ -21,17 +25,16 @@ public class Main {
 		for (int i = 0; i < R; i++) {
 			String s = br.readLine();
 			for (int j = 0; j < s.length(); j++) {
-				map[i][j] = s.charAt(j) - 'A';
+				map[i][j] = s.charAt(j) - 'A'; // 문자를 숫자로 변환하여 저장
 			}
 		}
-		check[map[0][0]] = true;
+		check[map[0][0]] = true; // 첫 시작 알파벳 방문 체크
 		dfs(0, 0, 1);
 		System.out.println(count);
 	}
 	
 	private static void dfs(int r, int c, int cnt) {
-		count = Math.max(count, cnt);
-//		System.out.println((char)(map[r][c] + 'A'));
+		count = Math.max(count, cnt); // 최대 방문 갯수 저장
 		
 		for (int d = 0; d < 4; d++) {
 			int nr = r + dr[d];
