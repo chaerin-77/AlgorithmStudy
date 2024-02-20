7c4cfdf85d5a805373b1f0e7dc536579d9d46e29
조성정 <jsj046@naver.com>
2024-02-20 11:33:28 +0900

    폴더 수정

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -5,51 +5,46 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int L, C;
-	static char[] input, picked;
 	static StringBuilder sb = new StringBuilder();
+	static int L, C;
+	static char[] character, picked;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		L = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		picked = new char[L]; // 최종으로 선택할 배열 - 길이 L
-		input = new char[C]; // 입력 받을 배열
+		character = new char[C];
+		picked = new char[L];
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < C; i++) {
-			input[i] = st.nextToken().charAt(0);
+			character[i] = st.nextToken().charAt(0);
 		}
-		Arrays.sort(input);
-		password(0, 0);
+		// 사전순으로 빠른 알파벳부터 찾기 위해 정렬
+		Arrays.sort(character);
+		comb(0, 0);
 		System.out.println(sb);
 	}
 	
-	public static void password(int cnt, int start) {
+	public static void comb (int cnt, int start) {
 		if (cnt == L) {
-			if (check(picked)) {
-				Arrays.sort(picked);
-				sb.append(String.valueOf(picked));
-				sb.append("\n");
+			String vowel = "aeiou";
+			int v = 0, c = 0;
+			for (int i = 0; i < L; i++) {
+				// 모음과 자음 갯수 count
+				if (vowel.contains(String.valueOf(picked[i]))) v++;
+				else c++;
 			}
+			// 모음이 1개 이상, 자음이 2개 이상이면 출력
+			if(v > 0 && c > 1) sb.append(String.valueOf(picked) + "\n");
+			
 			return;
 		}
 		
 		for (int i = start; i < C; i++) {
-			picked[cnt] = input[i];
-			password(cnt + 1, i + 1);
+			picked[cnt] = character[i];
+			comb(cnt + 1, i + 1);
 		}
-	}
-	
-	public static boolean check (char[] picked) {
-		String test = "aeiou";
-		int vowel = 0;
-		for (int i = 0; i < L; i++) {
-			// valueof는 char을 string으로 변환
-			if(test.contains(String.valueOf(picked[i]))) vowel ++;
-		}
-		
-		if (vowel < 1 || L - vowel < 2) return false;
-		else return true;
 	}
 }
