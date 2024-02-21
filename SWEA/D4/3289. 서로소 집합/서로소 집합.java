4fc0382eec9711194162345183126e40d57fd43c
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-21 16:50:59 +0900

    [D4] Title: 서로소 집합, Time: 465 ms, Memory: 101,928 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -23,10 +23,11 @@ public class Solution {
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				
+				// 0일 경우 union, 1일 경우 같은 집합인지 find
 				if (calc == 0) union(a, b);
 				else {
-					if (find(a) == find(b)) sb.append(1);
-					else sb.append(0);
+					if (find(a) == find(b)) sb.append(1); // 부모가 같으면 1
+					else sb.append(0); // 아니면 0
 				}
 			}
 			System.out.println("#" + t + " " + sb);
@@ -36,6 +37,7 @@ public class Solution {
 	private static void makeSet() {
 		parents = new int[N + 1];
 		
+		// 배열 생성 후 자기 자신을 부모로 설정
 		for (int i = 1; i <= N; i++) 
 			parents[i] = i;
 	}
@@ -44,12 +46,14 @@ public class Solution {
 		int rootA = find(a);
 		int rootB = find(b);
 		
-		parents[rootA] = rootB;
+		parents[rootA] = rootB; // b를 a의 부모로 설정
 	}
 	
 	private static int find(int a) {
+		// 자기 자신이 부모라면 return
 		if(parents[a] == a) return a;
 		
+		// 아니면 a의 부모로 재귀 탐색
 		return parents[a] = find(parents[a]);
 	}
 }
