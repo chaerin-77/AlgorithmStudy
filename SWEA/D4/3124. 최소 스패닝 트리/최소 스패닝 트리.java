d7b7a0a8658e33f0c0cfe22a6e39364b14bacf71
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-22 21:49:27 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,978 ms, Memory: 118,480 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -20,8 +20,8 @@ public class Solution {
         }
     }
     static int V, E;
-    static int[] parents;
-    static Edge[] edgeList;
+    static int[] parents; // 서로소 집합
+    static Edge[] edgeList; // 간선 정보 저장 리스트
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,7 +31,7 @@ public class Solution {
             st = new StringTokenizer(br.readLine());
             V = Integer.parseInt(st.nextToken());
             E = Integer.parseInt(st.nextToken());
-            edgeList = new Edge[E];
+            edgeList = new Edge[E]; // E크기의 간선 리스트 생성
             for (int i = 0; i < E; i++) {
                 st = new StringTokenizer(br.readLine());
                 int from = Integer.parseInt(st.nextToken());
@@ -39,19 +39,19 @@ public class Solution {
                 int weight = Integer.parseInt(st.nextToken());
                 edgeList[i] = new Edge(from, to, weight);
             }
-            Arrays.sort(edgeList);
+            Arrays.sort(edgeList); // 가중치에 따라 정렬
 
             parents = new int[V + 1];
             for (int i = 1; i <= V; i++) {
-                parents[i] = i;
+                parents[i] = i; // 자기 자신을 부모로 하는 집합 생성
             }
 
             int cnt = 0;
-            long minWeight = 0;
+            long minWeight = 0; // 가중치 합의 자료형은 long
             for (Edge e : edgeList) {
-                if (!union(e.from, e.to)) continue;
-                minWeight += e.weight;
-                if(++cnt == V - 1) break;
+                if (!union(e.from, e.to)) continue; // union에 실패 (부모가 같음) 시 다음 탐색
+                minWeight += e.weight; // 가중치 합
+                if(++cnt == V - 1) break; // 모든 정점을 탐색했기 때문에 break;
             }
             System.out.println(String.format("#%d %d", t, minWeight));
         }
@@ -63,12 +63,12 @@ public class Solution {
 
         if (rootA == rootB) return false;
 
-        parents[rootB] = rootA;
+        parents[rootB] = rootA; // 합집합
         return true;
     }
 
     private static int find(int a) {
         if (parents[a] == a) return a;
-        return parents[a] = find(parents[a]);
+        return parents[a] = find(parents[a]); // 경로 압축
     }
 }
