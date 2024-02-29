46820de011b8ad13bf677d40b00a078e6c65ff1b
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-02-29 13:16:46 +0900

    [Silver II] Title: 외판원 순회 2, Time: 324 ms, Memory: 12380 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,66 +1,46 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-/*
-N개의 도시들 (사이엔 길이 없을수도)
-한 도시에서 출발해 N개의 도시를 거쳐 다시 원래의 도시로 돌아옴
-한 번 갔던 도시로는 다시 갈 수 없음
-가장 적은 비용의 계획
-W[i][j] : i에서 j로 가는 비용 (비대칭)
-W[i][i] 는 항상 0
-W[i][j] 가 0이라면 가는 길이 없는 것
-
-입력
-N
-N개의 줄에 비용 행렬
-
-*문제 해결 프로세스*
-1. N개의 조합 생성 (중복 X)
-2. 비용을 확인하여 길이 없을 경우 return;
-3. 값이 있을 경우 전역 변수인 answer에 값 저장
-(answer은 값 저장 시 Math.min 사용)
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, answer = Integer.MAX_VALUE;
-	static int[][] city;
 	static int[] picked;
-	static boolean[] isSelected;
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = Integer.parseInt(sc.nextLine());
-		city = new int[N][N];
+	static int[][] W;
+	static int N, answer = Integer.MAX_VALUE;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		W = new int[N][N];
 		picked = new int[N + 1];
-		isSelected = new boolean[N];
 		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
-				city[i][j] = sc.nextInt();
+				W[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		road(0);
+		permu(0, 0);
 		System.out.println(answer);
 	}
-
-	private static void road(int cnt) {
+	
+	private static void permu (int cnt, int flag) {
 		if (cnt == N) {
-			// 마지막 도시에서 시작점으로 돌아가는 것 추가
 			picked[N] = picked[0];
-			int temp = 0;
+			int cost = 0;
 			for (int i = 0; i < N; i++) {
-				// 가는 길이 없으면 return
-				if (city[picked[i]][picked[i+1]] == 0) return;
-				temp += city[picked[i]][picked[i+1]];
+				int temp = W[picked[i]][picked[i+1]];
+				if (temp == 0) return;
+				cost += temp;
 			}
-			answer = Math.min(answer, temp);
+			answer = Math.min(answer, cost);
 			return;
 		}
+		
 		for (int i = 0; i < N; i++) {
-			if (isSelected[i]) continue;
-			isSelected[i] = true;
+			if ((flag & (1 << i)) != 0) continue;
 			picked[cnt] = i;
-			road(cnt + 1);
-			isSelected[i] = false;
+			permu(cnt + 1, flag | (1 << i));
 		}
 	}
 }
