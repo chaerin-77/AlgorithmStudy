59f79329d699e48c9cd8c43aa34b781e55777e9c
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-03-07 09:40:36 +0900

    Update 전화번호 목록.java

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -5,6 +5,9 @@ class Solution {
         boolean answer = true;
         
         Arrays.sort(phone_book);
+        
+        // 정렬이 되어있기 때문에 바로 다음의 문자만 확인하면 됨
+        // 앞부분이 같은지 순서로 정렬이 되어있기 때문
         for (int i = 0; i < phone_book.length - 1; i++) {
         	String check = phone_book[i];
         	String next = phone_book[i+1];
