3171880efd7e5bc584ef1e8be86ee8e78d174ff4
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-04-03 15:09:37 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,316 ms, Memory: 105,592 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -2,11 +2,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.StringTokenizer;
+
+/*
+ * 문제 해결 프로세스
+ * 1. 나의 순서를 알 수 있는 경우 -> 부모의 수와 자식의 수를 모두 알 때
+ * 2. 입력 시 역방향 리스트도 생성하여 사용
+ */
 
 public class Solution {
 	static int N, M, answer, cnt;
@@ -32,16 +35,17 @@ public class Solution {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				students[a].add(b);
-				reverse[b].add(a);
+				students[a].add(b); // 정방향 리스트
+				reverse[b].add(a); // 역방향 리스튼
 			}
 			
 			for (int a = 1; a <= N; a++) {
 				cnt = 0;
 				visited = new boolean[N+1];
-				findParents(a);
-				findChildren(a);
-//				System.out.println(Arrays.toString(visited));
+				findParents(a); // 부모의 개수
+				findChildren(a); // 자식의 개수
+				
+				// 부모의 개수와 자식의 개수를 더했을 때 N-1과 같다면 정답
 				if (cnt == N-1) answer++;
 			}
 			
@@ -54,7 +58,7 @@ public class Solution {
 		visited[a] = true;
 		for (int b : reverse[a]) {
 			if (visited[b]) continue;
-			cnt++;
+			cnt++; // 자기 자신을 셀 수 있기 때문에 여기서 증가
 			findChildren(b);
 		}
 	}
@@ -63,7 +67,7 @@ public class Solution {
 		visited[a] = true;
 		for (int b : students[a]) {
 			if (visited[b]) continue;
-			cnt++;
+			cnt++; // 자기 자신을 셀 수 있기 때문에 여기서 증가
 			findParents(b);
 		}
 	}
