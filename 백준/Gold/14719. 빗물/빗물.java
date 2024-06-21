3a630d3cb14b062dd485b9629639804c4a60fbac
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-06-21 18:13:53 +0900

    [Gold V] Title: 빗물, Time: 80 ms, Memory: 11804 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -3,6 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+ * 문제 해결 프로세스
+ * 1. 왼쪽과 오른쪽을 확인하여 현재 칸에 얼마나 물이 고일 수 있는지 확인
+ * 2. 이후 더 낮은 높이 만큼 물이 고일 수 있음
+ */
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
